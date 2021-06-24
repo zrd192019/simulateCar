900c2482f557c5bc9f76e12ecc5d37d9dda4908b
zrd192019 <86355637+zrd192019@users.noreply.github.com>
2021-06-24 20:55:34 +0800

    Add files via upload

---
+++ src/model/Place.java
@@ -0,0 +1,60 @@
+package model;
+
+/**
+ * Place is the location of station, car or districts
+ * @author Zhanghaoji
+ * @date 2021.06.2021/6/21 22:22
+ */
+public class Place {
+
+    private String id;
+
+    /**
+     * type can be
+     * living  居住区
+     * industrial  工业区
+     * business  商业区
+     * cur  移动中
+     */
+    private String type;
+
+    private double X;
+
+    private double Y;
+
+    public Place(String id, String type, double x, double y) {
+        this.id = id;
+        this.type = type;
+        X = x;
+        Y = y;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getX() {
+        return X;
+    }
+
+    public double getY() {
+        return Y;
+    }
+    
+    public void setY(double y) {
+    	this.Y = y ;
+    }
+    public void setX(double x) {
+    	this.X = x ;
+    }
+    
+    public static double getDistanceOf(Place a, Place b) {
+        double dis = Math.abs(a.X - b.X) + Math.abs(a.Y - b.Y) ;
+        return dis;
+    }
+
+}
